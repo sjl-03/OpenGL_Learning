773060082869fbe14764c485e69c168ff33abac3
sjl-03 <shenjunlu03@gmail.com>
2024-06-03 16:41:12 +0100

    OpenGL Tutorial V8

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,6 +1,46 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+struct ShaderProgramSource
+{
+    std::string VertexSource;
+    std::string FragmentSource;
+};
+/*
+ * This reads the shader program in 
+ */
+static ShaderProgramSource ParseShader(const std::string& filepath)
+{
+    enum class ShaderType
+    {
+        NONE = -1, VERTEX = 0, FRAGMENT = 1
+    };
+    
+    std::ifstream stream(filepath);
+    std::string line;
+    std::stringstream ss[2];
+    ShaderType type = ShaderType::NONE;
+
+    while (getline(stream, line))
+    {
+        if (line.find("#shader") != std::string::npos) {
+            if (line.find("vertex") != std::string::npos)
+                type = ShaderType::VERTEX;
+            else if (line.find("fragment") != std::string::npos)
+                type = ShaderType::FRAGMENT;
+        }
+        else
+        {
+            ss[(int)type] << line << '\n'; // this way if the type is vertex, it adds to index 0 etc
+        }
+    }
+
+    return { ss[0].str(), ss[1].str() };
+}
 
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
@@ -98,27 +138,13 @@ int main(void)
     const int ATTRIBUTE_INDEX = 0;
     glVertexAttribPointer(ATTRIBUTE_INDEX, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
-    std::string vertexShader = // we can concatenate string in c++ in the following way
-        "#version 410 core\n"
-        "\n"
-        "layout(location = 0) in vec4 position;\n" // 0 is the ATTRIBUTE_INDEX
-        "\n"
-        "void main()\n"
-        "{\n"
-        "   gl_Position = position;\n"
-        "}\n";
+    ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
+    std::cout << "Vertex" << std::endl;
+    std::cout << source.VertexSource << std::endl;
+    std::cout << "Fragment" << std::endl;
+    std::cout << source.FragmentSource << std::endl;
 
-    std::string fragmentShader = // we can concatenate string in c++ in the following way
-        "#version 410 core\n"
-        "\n"
-        "layout(location = 0) out vec4 color;\n" // 0 is the ATTRIBUTE_INDEX
-        "\n"
-        "void main()\n"
-        "{\n"
-        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
-        "}\n";
-
-    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     glUseProgram(shader);
 
 
